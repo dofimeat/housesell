37a95dd4a171f968e85ad3a88ae5ed8e2029f8c1
dofimeat <dofmorphius222@gmail.com>
2024-07-11 14:25:32 +0300

    add adap. and update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
-'use strict';
-
-const menuIcon = document.getElementById("menu-icon");
-const navMenu = document.querySelector(".navlist");
-
-// Обработчик события для кнопки меню
-menuIcon.addEventListener("click", function() {
-    navMenu.classList.toggle("active");
-});
-
-
-// const /** {NodeElement} */ $navbar = document.querySelector(".navbar-list");
-// const /** {NodeElement} */ $navToggler = document.querySelector(".icon-btn");
-
-// $navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
-
-// const /** {NodeElement} */ $header = document.querySelector(".navbar");
-
-// window.addEventListener("scroll", () => {
-//     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const mobileNavlist = document.querySelector(".mobile-navlist");
+    const navLinks = document.querySelectorAll(".navlist a");
+  
+    menuIcon.addEventListener("click", () => {
+      mobileNavlist.classList.toggle("open");
+      menuIcon.classList.toggle("open");
+    });
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        mobileNavlist.classList.remove("open");
+        menuIcon.classList.remove("open");
+      });
+    });
+  });
